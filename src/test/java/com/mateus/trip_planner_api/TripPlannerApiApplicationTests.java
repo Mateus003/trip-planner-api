55d9338f01f55f5484cc2ab6492f074fd94f74e9
Mateus <mateusdev0101@gmail.com>
2025-11-04 01:49:24 -0300

    test: adiciona estrutura inicial de testes

---
+++ src/test/java/com/mateus/trip_planner_api/TripPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.trip_planner_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
